f5868cf9212d52057ab0a7111f3b35ec3a88f014
Nikolai Kotok <lennakz@gmail.com>
2017-10-05 14:32:13 -0700

    Add javascript

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+	
+	$(".nav-item a").on('click', function (event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+			var hash = this.hash;
+
+			$('html, body').animate({
+				scrollTop: $(hash).offset().top
+			}, 800, function () {
+
+				window.location.hash = hash;
+			});
+		}  
+	});
+	
+	$(window).on('scroll', function() {
+		if ($('body').scrollTop > 100 || document.documentElement.scrollTop > 100) {
+			$('#scroll-to-top').css('display', 'block');
+		} else {
+			$('#scroll-to-top').css('display', 'none');
+		}
+	});
+
+	$('#scroll-to-top').on('click', function() {
+		$('html, body').animate({
+			scrollTop: 0
+			}, 800
+		);
+	});
+});
